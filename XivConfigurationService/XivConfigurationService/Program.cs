0119eece6ff26eeedebb1743232f69f99f4f27db
Tawmy <git@tawmy.dev>
2025-04-07 08:58:04 +0200

    Add solution and project files

---
+++ XivConfigurationService/XivConfigurationService/Program.cs
@@ -0,0 +1,32 @@
+using XivConfigurationService.Components;
+using _Imports = XivConfigurationService.Client._Imports;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(_Imports).Assembly);
+
+app.Run();
